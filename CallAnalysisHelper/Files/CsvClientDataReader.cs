c2ac891216536bfc5e43e26c4a019298bcfea50a
burstCode <yourewelcomex@gmail.com>
2024-08-16 08:43:02 +0300

    Добавлены парсеры Excel и CSV файлов

---
+++ CallAnalysisHelper/Files/CsvClientDataReader.cs
@@ -0,0 +1,48 @@
+﻿using CallAnalysisHelper.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace CallAnalysisHelper.Files
+{
+    public class CsvClientDataReader
+    {
+        public List<Client> ReadClientDataFromCsv(string filePath)
+        {
+            var clients = new List<Client>();
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                Delimiter = ","
+            };
+
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Read();
+                csv.ReadHeader();
+
+                while (csv.Read())
+                {
+                    var id = csv.GetField<int>("ID");
+                    var companyName = csv.GetField<string>("Название компании");
+                    var phoneNumbers = csv.GetField<string>("Рабочий телефон");
+
+                    clients.Add(new Client
+                    {
+                        Id = id,
+                        CompanyName = companyName,
+                        PhoneNumbers = phoneNumbers
+                    });
+                }
+            }
+
+            return clients;
+        }
+    }
+}
---
+++ CallAnalysisHelper/Files/ExcelCallDataReader.cs
@@ -0,0 +1,53 @@
+﻿using CallAnalysisHelper.Models;
+using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CallAnalysisHelper.Files
+{
+    public class ExcelCallDataReader
+    {
+        public List<CallRecord> ReadCallDataFromExcel(string filePath)
+        {
+            var callRecords = new List<CallRecord>();
+
+            // Настраиваем лицензию для библиотеки
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0]; // Выбираем лист с отчетом
+
+                // Пропускаем ненужные строки и идем до последней строки листа
+                for (int row = 11; row <= worksheet.Dimension.Rows; row++)
+                {
+                    var isMissed = worksheet.Cells[row, 1].Text == "пропущенный" ? true : false;
+
+                    var clientPhoneNumber = worksheet.Cells[row, 2].Text;
+
+                    var supportAgentName = worksheet.Cells[row, 3].Text;
+
+                    var callDate = DateTime.Parse(worksheet.Cells[row, 8].Text);
+
+                    var callTime = DateTime.Parse(worksheet.Cells[row, 9].Text);
+
+                    var callDuration = TimeSpan.Parse(worksheet.Cells[row, 11].Text);
+
+                    // Добавляем запись
+                    callRecords.Add(new CallRecord
+                    {
+                        IsMissed = isMissed,
+                        ClientPhoneNumber = clientPhoneNumber,
+                        SupportAgentName = supportAgentName,
+                        CallDate = callDate,
+                        CallTime = callTime,
+                        CallDuration = callDuration,
+                    });
+                }
+            }
+
+            return callRecords;
+        }
+    }
+}
